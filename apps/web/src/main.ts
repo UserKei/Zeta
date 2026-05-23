5c7bcf2edbb9761a0b8f0baecb1c86db9945e63f
User_Kei <user_kei@outlook.com>
2026-05-23 17:40:15 +0800

    build(web): add frontend ui dependencies

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -2,13 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(ElementPlus, { locale: zhCn })
 app.use(router)
 
 app.mount('#app')
